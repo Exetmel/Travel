58a58aed5fc8cd7329f0e5b22a4cf9f6d40fe7a9
Exetmel <124749784+Exetmel@users.noreply.github.com>
2023-04-29 11:20:30 +0800

    Add files via upload

---
+++ Travel Blog 3rd/home.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0";>
+    <title>home</title>
+
+
+<!--swiiper css link -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
+
+<!--font awesome cdn link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+
+
+<!--custom css file link -->
+<link rel="stylesheet" href="style.css">
+
+
+
+
+
+</head>
+<body>
+    
+<!-- header section starts -->
+
+<section class="header">
+
+    <a href="home.php" class="logo">Travel blog</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends --> 
+
+
+
+
+<!-- home section starts -->
+
+<section class="home">
+
+    <div class="home slider">
+
+        <div class="w">
+
+            <div class="slide" style="background:url(images/p-1.jpg) no-repeat">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3> travel around the philippines</h3>
+                    <a href="package.php" class="btn">discover more</a>  
+                </div>              
+            </div>
+
+            <div class="slide" style="background:url(images/p-2.jpg) no-repeat">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3> discover the beaches </h3>
+                    <a href="package.php" class="btn">discover more</a>  
+                </div>              
+            </div>
+
+            <div class="slide" style="background:url(images/p-3.jpg) no-repeat">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3> make your tour worthwhile</h3>
+                    <a href="package.php" class="btn">discover more</a>  
+                </div>              
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+</section>
+
+
+
+
+
+<!-- home section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- service section starts -->
+
+<section class="services">
+
+<h1 class="heading-title"> our services </h1>
+<div class="box-container">
+
+    <div class="box">
+      <img src="images/sleep.png" alt=""> 
+      <h3>hotel</h3>
+    </div>
+
+    <div class="box">
+      <img src="images/food.png" alt=""> 
+      <h3>Food and Drinks</h3>
+    </div>
+
+    <div class="box">
+      <img src="images/safety.png" alt=""> 
+      <h3>Safety Guide</h3>
+    </div>
+
+    <div class="box">
+      <img src="images/travel.png" alt=""> 
+      <h3>Fastest Travel</h3>
+    </div>
+
+    <div class="box">
+      <img src="images/adventure.png" alt=""> 
+      <h3>Adventure</h3>
+    </div>
+
+    <div class="box">
+      <img src="images/philippines.png" alt=""> 
+      <h3>Around the Philippines</h3>
+    </div>
+
+</div>
+
+</section>
+
+
+
+<!-- service section ends -->
+
+
+
+
+
+
+
+
+
+<!-- footer section starts --> 
+
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class=" fas fa-angle-right"></i>home</a>
+        <a href="about.php"> <i class=" fas fa-angle-right"></i>about</a>
+        <a href="package.php"> <i class=" fas fa-angle-right"></i>package</a>
+        <a href="book.php"> <i class=" fas fa-angle-right"></i>book</a>
+    </div>
+
+
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class=" fas fa-angle-right"></i>ask questions</a>
+        <a href="#"> <i class=" fas fa-angle-right"></i>about us</a>
+        <a href="#"> <i class=" fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"> <i class=" fas fa-angle-right"></i>terms of use</a>
+    </div>
+
+
+    
+    <div class="box">
+        <h3>contact info </h3>
+        <a href="#"> <i class=" fas fa-phone"></i> +123-456-7890 </a>
+        <a href="#"> <i class=" fas fa-phone"></i> +11-222-3333  </a>
+        <a href="#"> <i class=" fas fa-envelope"></i> mirishiki@gmail.com</a>
+        <a href="#"> <i class=" fas fa-map"></i> Philippines, Mindanao</a>
+    </div> 
+
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class=" fab fa-facebook-f"></i> facebook </a>
+    </div>
+
+</div>
+
+<div class="credit"> This Site Created on <span>April 28, 2023</span> | Book now! </div> 
+
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- custom js file link-->
+
+<script src="script.js"></script>
+
+
+</body>
+</html>
