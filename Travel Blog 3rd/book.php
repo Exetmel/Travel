748812d78512db3c195c4b8e323110d348ca4a1c
Kent <melkennethexclamador@gmail.com>
2023-05-11 14:35:00 +0800

    Book form DB added

--- Travel Blog 3rd/book.php
+++ Travel Blog 3rd/book.php
@@ -54,45 +54,48 @@
 
 <section class="booking">
     <h1 class="heading-title">book your trip!</h1>
-    <form action="" class="book_form.php" methot="post" class="book-form">
+
+    <form action="book_form.php" methot="post" class="book-form">
+
     <div class="flex">
-        <div class="input-box">
-            <span>name:</span>
+
+        <div class="inputBox">
+            <span>name :</span>
             <input type="text" placeholder="enter your name" name="name">
         </div>
 
-        <div class="input-box">
-            <span>email:</span>
+        <div class="inputBox">
+            <span>email :</span>
             <input type="email" placeholder="enter your email" name="email">
         </div>
 
-        <div class="input-box">
-            <span>phone:</span>
-            <input type="number" placeholder="enter your phone number" name="phone">
+        <div class="inputBox">
+            <span>phone :</span>
+            <input type="number" placeholder="enter your number" name="phone">
         </div>
 
-        <div class="input-box">
-            <span>address:</span>
+        <div class="inputBox">
+            <span>address :</span>
             <input type="text" placeholder="enter your address" name="address">
         </div>
 
-        <div class="input-box">
-            <span>where to:</span>
+        <div class="inputBox">
+            <span>where to :</span>
             <input type="text" placeholder="place you want to visit" name="location">
         </div>
 
-        <div class="input-box">
-            <span>how many:</span>
+        <div class="inputBox">
+            <span>how many :</span>
             <input type="number" placeholder="number of guests" name="guests">
         </div>
 
-        <div class="input-box">
-            <span>arrivals:</span>
+        <div class="inputBox">
+            <span>arrivals :</span>
             <input type="date" name="arrivals">
         </div>
 
-        <div class="input-box">
-            <span>leaving:</span>
+        <div class="inputBox">
+            <span>leaving :</span>
             <input type="date" name="leaving">
         </div>
     </div>
